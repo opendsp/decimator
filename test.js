353a46a5b81575707e0400383d09f0900671b97d
Potasmic <khoinguyen998@gmail.com>
2015-06-14 18:55:35 -0700

    test fix

--- test.js
+++ test.js
@@ -8,10 +8,10 @@ import dbg from 'debug';
 
 dbg('SampleRate')(sampleRate);
 
-var test = new Decimator(1.8*100);
+var test = new Decimator(1.8176*50);
 
 export function dsp(t) {
   var sine = 0.3 * Math.sin( Math.PI * 2 * t * 329);
   
-  return test.run(sine);
+  return test.setFactor(1.8176 * 10 * (t%10)).run(sine);
 }
